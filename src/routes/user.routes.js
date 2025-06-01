b861921a21224698decffa62de0b9f7dbb9395c6
Ashutosh Kumar <pathakashutoshdmr@gmail.com>
2025-06-01 23:04:08 +0530

    Add getWatchHistory endpoint to fetch user's watch history with video owner details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeUserPassword,
+            getCurrentUser,
+            getUserChannelProfile,
+            getWatchHistory, loginUser, 
+            logoutUser,
+            refreshAccessToken,
+            registerUser,
+            updateAccountDetails,
+            updateUserAvatar, 
+            updateUserCoverImage 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -23,6 +33,14 @@ router.route("/login").post(loginUser)
 //secured Routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeUserPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:userName").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 export default router
